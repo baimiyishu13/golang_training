4f59dba2b3fd87517459dd343d38d1f3696a211a
baimiyishu13 <baimiyishu13@163.com>
2023-12-20 23:04:47 +0800

    Making HTTP calls with net/http

--- CODE/gitlab/gitlab.go
+++ CODE/gitlab/gitlab.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	resp, err := http.Get("http://gitlab.com/users/baimiyishu13")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		log.Fatalf("error: %s", err)
 		/*
@@ -16,5 +18,11 @@ func main() {
 	}
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf("error: %s", resp.Status)
+	}
+
+	//fmt.Printf("Header: %s\n", resp.Header.Get("Content-Type"))
+
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatalf("error: copy - %s", err)
 	}
 }

f9e10e4bd9f95a31e290d3d290171d15e47ecee6
baimiyishu13 <baimiyishu13@163.com>
2023-12-21 20:06:32 +0800

    Making HTTP calls with net/http - Serializing JSON

--- CODE/gitlab/gitlab.go
+++ CODE/gitlab/gitlab.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -22,7 +22,44 @@ func main() {
 
 	//fmt.Printf("Header: %s\n", resp.Header.Get("Content-Type"))
 
-	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-		log.Fatalf("error: copy - %s", err)
+	//if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+	//	log.Fatalf("error: copy - %s", err)
+	//}
+
+	var r Reply
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&r); err != nil {
+		log.Fatalf("error: %s", err)
 	}
+
+	//fmt.Println(r)
+	fmt.Printf("%#v\n", r)
 }
+
+func jsonInfo(url string) (string, int, error) {
+	//TODO: you code goes here
+
+	return "", 0, nil
+}
+
+type Reply struct {
+	UserID    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+/* Json -> Go
+true/false	-	true/false
+string	-	string
+null	-	nil
+number	-	int ... uint ... float ...
+array	-	[]any	([]interface{})
+object	-	map[string]an,struct
+
+JSON - io.Reader->	Go:JSON.Decode
+JSON - []byte	-> GO: JSON.Unmarshal
+
+Go ->io.Writer-> JSON: json.Encoder
+Go -[]byte -> Go: json.Marshal
+*/
